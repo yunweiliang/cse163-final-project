249ffbde2c148541e4015fadb3acb5974de80fc1
Yunwei Liang <yunweilia@gmail.com>
2020-03-16 21:21:58 -0700

    pass flake8

--- analyze_ml_models.py
+++ analyze_ml_models.py
@@ -18,7 +18,6 @@ from sklearn.naive_bayes import GaussianNB
 
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
-from sklearn import tree
 from sklearn.tree import export_graphviz
 
 from sklearn.model_selection import KFold, cross_val_score
@@ -32,7 +31,8 @@ class ML_Model:
         """
         self._data = df
         self._data = self._data.dropna().reset_index(drop=True)
-        self._data.loc[self._data['prediction'] > 0, 'prediction'] = 1 # Make into binary problem
+        self._data.loc[self._data['prediction'] > 0,
+                       'prediction'] = 1  # Make into binary problem
         self._feature_importances = None
 
     def get_clean_data(self):
@@ -46,7 +46,7 @@ class ML_Model:
         Takes in an optional x_exempt argument and trains a Decision Tree
         Classifier with all attributes except x_exempt as the features
         and prediction as the label. Outputs a decision_tree_model.pdf
-        of the model visualization. Returns the accuracy of model. 
+        of the model visualization. Returns the accuracy of model.
         """
         X = self._data.loc[:, self._data.columns != 'prediction']
         if x_exempt is not None:
@@ -54,13 +54,14 @@ class ML_Model:
         X = pd.get_dummies(X)
         y = self._data['prediction']
 
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+        X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                            test_size=0.2)
         model = DecisionTreeClassifier()
         model = model.fit(X_train, y_train)
 
         graph = self._plot_tree(model, X=X, y=y)
-        graph.render('decision_tree_model') # save as pdf
-        
+        graph.render('decision_tree_model')  # save as pdf
+
         score = accuracy_score(y_test, model.predict(X_test))
         return score
 
@@ -69,7 +70,7 @@ class ML_Model:
         Takes in an optional x_exempt argument and trains a Random Forest
         Classifier with all attributes except x_exempt as the features
         and prediction as the label. Outputs a random_forest_model.pdf
-        of the model visualization. Returns the accuracy of model. 
+        of the model visualization. Returns the accuracy of model.
         """
         # save copy of dataframe format for plotting
         X = self._data.loc[:, self._data.columns != 'prediction']
@@ -81,18 +82,23 @@ class ML_Model:
         # select info and change to numpy format for random forest modeling
         X_np = np.array(X)
         y_np = np.array(y)
-        X_train, X_test, y_train, y_test = train_test_split(X_np, y_np, test_size=0.2)
+        X_train, X_test, y_train, y_test = train_test_split(X_np, y_np,
+                                                            test_size=0.2)
         model = RandomForestClassifier(n_estimators=1000, random_state=42)
         model.fit(X_train, y_train)
 
-        graph = self._plot_tree(model.estimators_[10], X=X, y=y) # only select five trees to display
+        graph = self._plot_tree(model.estimators_[10],
+                                X=X, y=y)  # only select five trees to display
         graph.render('random_forest_model')
 
         # save feature importances for future analysis
         importances = list(model.feature_importances_)
-        feature_importances = [(feature, round(importance, 2)) for feature, importance in zip(list(columns), importances)]
-        self._feature_importances = sorted(feature_importances, key=lambda x:x[1], reverse=True)
-        #[print('Variable: {} Importance: {}'.format(*pair)) for pair in feature_importances]
+        feature_importances = [(feature, round(importance, 2)) for feature,
+                               importance in zip(list(columns), importances)]
+        self._feature_importances = sorted(feature_importances,
+                                           key=lambda x: x[1], reverse=True)
+        # [print('Variable: {} Importance: {}'.format(*pair)) \
+        # for pair in feature_importances]
 
         score = accuracy_score(y_test, model.predict(X_test))
         return score
@@ -101,7 +107,7 @@ class ML_Model:
         """
         Takes in an optional x_exempt argument and trains a Gaussian Naive
         Bayes Classifier with all attributes except x_exempt as the features
-        and prediction as the label. Returns the accuracy of model. 
+        and prediction as the label. Returns the accuracy of model.
         """
         X = self._data.loc[:, self._data.columns != 'prediction']
         if x_exempt is not None:
@@ -109,7 +115,8 @@ class ML_Model:
         X = pd.get_dummies(X)
         y = self._data['prediction']
 
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+        X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                            test_size=0.2)
         model = GaussianNB()
         model.fit(X_train, y_train)
 
@@ -121,12 +128,12 @@ class ML_Model:
         Takes in optional n and optional x_exempt arguments. Runs n trials
         for each of the three models with all attributes except x_exempt
         as the features and prediction as the label. Returns a dictionary
-        of model type mapped to their average accuracy scores. 
+        of model type mapped to their average accuracy scores.
         """
         trials_df = self.run_trials(n=n, x_exempt=x_exempt)
-        means = {'decision_tree': sum(trials_df.loc[:,'decision_tree']) / n,
-                 'forest': sum(trials_df.loc[:,'forest'])/n,
-                 'naive_bayes': sum(trials_df.loc[:,'naive_bayes']) / n}
+        means = {'decision_tree': sum(trials_df.loc[:, 'decision_tree']) / n,
+                 'forest': sum(trials_df.loc[:, 'forest'])/n,
+                 'naive_bayes': sum(trials_df.loc[:, 'naive_bayes']) / n}
         return means
 
     def models_performances_box_plot(self, n=10):
@@ -135,21 +142,23 @@ class ML_Model:
         three models with all attributes as the features and prediction
         as the label. Outputs a box_plot.png file of box plots comparing
         the accuracy scores of the three models. Returns a dictionary
-        of model type mapped to their average accuracy scores. 
+        of model type mapped to their average accuracy scores.
         """
         sns.set(style='whitegrid')
         trials_df = self.run_trials()
         sns.boxplot(data=trials_df)
-        means = {'decision_tree': sum(trials_df.loc[:,'decision_tree']) / n,
-                 'forest': sum(trials_df.loc[:,'forest']) / n,
-                 'naive_bayes': sum(trials_df.loc[:,'naive_bayes']) / n}
+        means = {'decision_tree': sum(trials_df.loc[:, 'decision_tree']) / n,
+                 'forest': sum(trials_df.loc[:, 'forest']) / n,
+                 'naive_bayes': sum(trials_df.loc[:, 'naive_bayes']) / n}
         plt.title('Model Performance over ' + str(n) + ' Trials')
-        plt.xticks((0, 1, 2), ('Decision Tree', 'Random Forest', 'Naive Bayes'))
+        plt.xticks((0, 1, 2), ('Decision Tree',
+                               'Random Forest',
+                               'Naive Bayes'))
         plt.xlabel('Models')
         plt.ylabel('Score')
         plt.savefig('box_plot.png')
         return means
-     
+
     def run_trials(self, n=10, x_exempt=None):
         """
         Takes in optional n and optional x_exempt arguments. Runs n trials
@@ -158,10 +167,10 @@ class ML_Model:
         of model as column names and each trial's accuracy recorded in
         respective columns.
         """
-        data = {'decision_tree':np.zeros(n),
-                'forest':np.zeros(n),
-                'naive_bayes':np.zeros(n)}
-        for i in range(n):  
+        data = {'decision_tree': np.zeros(n),
+                'forest': np.zeros(n),
+                'naive_bayes': np.zeros(n)}
+        for i in range(n):
             data['decision_tree'][i] = self.decision_tree(x_exempt)
             data['naive_bayes'][i] = self.naive_bayes(x_exempt)
             data['forest'][i] = self.forest(x_exempt)
@@ -189,7 +198,7 @@ class ML_Model:
         for i in val:
             sum += i
         return sum/len(val)
-    
+
     def _plot_tree(self, model, X, y):
         """
         This function takes a model and the X and y values for a dataset
@@ -197,9 +206,9 @@ class ML_Model:
 
         This function won't work with your cse163 environment.
         """
-        dot_data = export_graphviz(model, out_file=None, 
-                            feature_names=X.columns,  
-                            class_names=str(y.unique()),  
-                            filled=True, rounded=True,  
-                            special_characters=True) 
+        dot_data = export_graphviz(model, out_file=None,
+                                   feature_names=X.columns,
+                                   class_names=str(y.unique()),
+                                   filled=True, rounded=True,
+                                   special_characters=True)
         return graphviz.Source(dot_data)
--- main.py
+++ main.py
@@ -6,63 +6,65 @@
 from analyze_ml_models import ML_Model
 import numpy as np
 import pandas as pd
-from scipy.stats import pearsonr 
+from scipy.stats import pearsonr
 import seaborn as sns
 import matplotlib.pyplot as plt
-
 
 
 def correlation(clean_data):
     corr = {}
     y = np.array(clean_data.loc[:, 'prediction'])
-    data =  clean_data.drop('prediction', axis=1)
+    data = clean_data.drop('prediction', axis=1)
     col_name = list(data.columns)
     for i in col_name:
         x = np.array(data.loc[:, i])
-        corr[i] = pearsonr(x,y)
-    df = pd.DataFrame(corr, index=['Correlation Coefficient', '2 Tailed P-Value'])
+        corr[i] = pearsonr(x, y)
+    df = pd.DataFrame(corr, index=['Correlation Coefficient',
+                                   '2 Tailed P-Value'])
     return df
 
-def plot_feature_importance(model, xes_exempt):
-    df = pd.DataFrame(columns=['Feature', 'Contains Feature', 'Decision Tree', 'Random Forest', 'Naive Bayes'])
-    n = 5 # default five trials
 
-    ######################## comment next 2 lines to run for all features ###########################
-    # xes_exempt = ['age', 'sex'] 
+def plot_feature_importance(model, xes_exempt):
+    df = pd.DataFrame(columns=['Feature', 'Contains Feature',
+                               'Decision Tree', 'Random Forest',
+                               'Naive Bayes'])
+    n = 5  # default five trials
+
+    # comment next 2 lines to run for all features
+    # xes_exempt = ['age', 'sex']
     # n = 1
 
     for x_exempt in xes_exempt:
         with_dict = model.calculate_mean_accuracy(n=n, x_exempt=x_exempt)
         without_dict = model.calculate_mean_accuracy(n=n, x_exempt=x_exempt)
-        with_row = {'Feature':x_exempt, 'Contains Feature':True, 
-                    'Decision Tree':with_dict['decision_tree'],
-                    'Random Forest':with_dict['forest'],
-                    'Naive Bayes':with_dict['naive_bayes']}
-        without_row = {'Feature':x_exempt, 'Contains Feature':False, 
-                    'Decision Tree':without_dict['decision_tree'],
-                    'Random Forest':without_dict['forest'],
-                    'Naive Bayes':without_dict['naive_bayes']}
+        with_row = {'Feature': x_exempt, 'Contains Feature': True,
+                    'Decision Tree': with_dict['decision_tree'],
+                    'Random Forest': with_dict['forest'],
+                    'Naive Bayes': with_dict['naive_bayes']}
+        without_row = {'Feature': x_exempt, 'Contains Feature': False,
+                       'Decision Tree': without_dict['decision_tree'],
+                       'Random Forest': without_dict['forest'],
+                       'Naive Bayes': without_dict['naive_bayes']}
         df = df.append(with_row, ignore_index=True)
         df = df.append(without_row, ignore_index=True)
     df = df.melt(id_vars=['Feature', 'Contains Feature'],
                  value_vars=['Decision Tree', 'Random Forest', 'Naive Bayes'],
                  var_name='model', value_name='Accuracy Score')
     graph = sns.catplot(x='Feature', y='Accuracy Score', col='model', data=df,
-                kind='bar', hue='Contains Feature', col_wrap=3)
+                        kind='bar', hue='Contains Feature', col_wrap=3)
     graph = graph.set_xticklabels(rotation=30)
     plt.subplots_adjust(top=0.9)
     graph.fig.suptitle('Performance of Models with vs. without a Feature')
     plt.savefig('features_performances_in_models.png')
     return df
 
-    
 
 def main():
     data = pd.read_csv('cleveland_processed.csv')
     model = ML_Model(data)
     clean_data = model.get_clean_data()
     clean_data.to_csv('clean_data.csv', index=False)
-    
+
     print('Decision Tree Score:', model.decision_tree())
     print('Gaussian Naive Bayes Score:', model.naive_bayes())
     print('Random Forest Score:', model.forest())
@@ -75,6 +77,7 @@ def main():
     print('Decision Tree Mean Score:', mean_accuracy['decision_tree'])
     print('Gaussian Naive Bayes Mean Score:', mean_accuracy['naive_bayes'])
     print('Random Forest Mean Score:', mean_accuracy['forest'])
+    print()
 
     print('Box Plot Means:', model.models_performances_box_plot())
     print()
@@ -82,10 +85,13 @@ def main():
     feature_correlations = correlation(clean_data)
     print(feature_correlations)
     feature_correlations.to_csv('feature_correlations_to_prediction.csv')
-    print(model.cross_validation())
     print()
-    
-    feature_importances = plot_feature_importance(model, clean_data.columns[clean_data.columns != 'prediction'])
+
+    print('Cross Validation:', model.cross_validation())
+    print()
+
+    feature_importances = plot_feature_importance(model,
+        clean_data.columns[clean_data.columns != 'prediction'])
     print(feature_importances)
     feature_importances.to_csv('features_performances_in_models.csv')
 
